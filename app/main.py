fc100ec3766c9079025e8fbd0b7b1234b8925f1e
paul_hazy <pavelpv8@gmail.com>
2021-02-08 23:24:17 +0400

    update

--- app/main.py
+++ app/main.py
@@ -4,18 +4,14 @@ from flask import request
 
 app = Flask(__name__)
 
-#@app.route("/")
-#def hello():
-#    return render_template("index.html")
 @app.route("/")
 def test():
-    return '<form action="/echo" method="POST"><input name="text"><input type="submit" value="Echo"></form>'
+    return render_template('index.html')
 
 @app.route("/echo", methods=["POST"])
 def test_post():
     return request.form['text']
 #    ar = request.args.get('txt')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
